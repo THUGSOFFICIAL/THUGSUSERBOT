695fd2f91c0cc5e0b8681369bd524c8fed73f82d
Gangster <72122792+THUGSOFFICIAL@users.noreply.github.com>
2020-10-12 23:13:39 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -5,21 +5,21 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
 PM_IMG = "https://telegra.ph/file/4c7467a0f49659007c60d.mp4"
-pm_caption = "🔱 **GANGSTER NEVER DIE 😎😎** 🔱\n\n"
+pm_caption = "🥰🥰**THUGBOT IS ONLINE DUDE** 🥰🥰\n\n"
 
-pm_caption += f"🔸🔹 **THUG USER**     :   {DEFAULTUSER}\n"
+pm_caption += f"💳💳 **MASTER**💳💳     :   {DEFAULTUSER}\n"
 
-pm_caption += "🔹🔸 TELETHON VERSION   :   1.15.0 \n"
+pm_caption += "🙏🙏**SUPPORT**🙏🙏      :   [JOIN](https://t.me/THUGUSERBOT)\n"
 
-pm_caption += "🔸🔹 OFFICIAL CHANNEL   :   [JOIN](https://t.me/THUGUSERBOT)\n"
+pm_caption += "🤖🤖**UPDATES**          :   SOON \n"
 
-pm_caption += "🔹🔸 OFFICIAL GROUL     :   SOON \n"
+pm_caption += "👹👹**SPAM GRP**👹👹     :.  [SPAM](https://t.me/joinchat/UKclkxutUAu1SGLHZ9hdkw)
+      
+pm_caption += "🗽🗽**MY GOD**🗽🗽       :   [THUGS](https://t.me/THUGS_OFFICIAL)\n"
 
-pm_caption += "🔹🔸 COPY RIGHT         :   [THUGS](https://t.me/THUGS_OFFICIAL)\n"
-
-#@command(outgoing=True, pattern="^.alive$")
-@borg.on(admin_cmd(pattern=r"alive"))
-async def amireallyalive(alive):
+#@command(outgoing=True, pattern="^.online$")
+@borg.on(admin_cmd(pattern=r"online"))
+async def amireallyalive(online):
     chat = await alive.get_chat()
     await alive.delete()
     """ For .alive command, check if the bot is running.  """
