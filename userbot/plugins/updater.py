da260c5ba568782ea125337b55fc1397bafd77e0
Gangster <72122792+THUGSOFFICIAL@users.noreply.github.com>
2020-10-16 12:54:22 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -20,7 +20,7 @@ requirements_path = path.join(
 HEROKU_API_KEY = Var.HEROKU_API_KEY
 HEROKU_APP_NAME = Var.HEROKU_APP_NAME
 GIT_REPO_NAME = "THUGSUSERBOT"
-UPSTREAM_REPO_URL = "https://github.com/THUGSOFFICIAL/THUGSUSERBOT"
+UPSTREAM_REPO_URL = "https://github.com/THUGSOFFICIAL/THUGSUSERBOT.git"
 
 async def gen_chlog(repo, diff):
     ch_log = ''
